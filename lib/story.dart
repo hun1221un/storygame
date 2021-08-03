0c2b3d0341243cafaa02c93adc9aa6c939d94a1a
hun1221un <nattanan.patchaipoon@gmail.com>
2021-08-03 16:52:59 +0700

    storygame v0.1

---
+++ lib/story.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Story{
+
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({String storyTitle, String choice1,String choice2}){
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+
+  }
+}
